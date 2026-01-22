a35490830754e7f94abbd8fa81754259ff066966
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-01-22 14:26:44 -0300

    Validando o token recebido

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -19,9 +19,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
-
         var subject = tokenService.getSubject(tokenJWT);
-        System.out.println(subject);
 
         filterChain.doFilter(request, response);
     }
